9f57cdd8e2aac24bd1f61a9c4e22e4450ce7e054
Avinash Choudhary <55015213+Avi-nash5@users.noreply.github.com>
2019-10-09 10:47:02 +0530

    Add files via upload

---
+++ Contests/Prime_Subtraction.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+	int T;
+	cin>>T;
+	for(int i=0; i<T; i++){
+		long long int a,b;
+		cin>>a>>b;
+		if(a-b ==1)
+			cout<<"NO \n";
+		else
+			cout<<"YES \n";
+	}
+}
